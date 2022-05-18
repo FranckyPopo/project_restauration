e7e0781576eae4629a85403947b48cba0c9cf866
FranckyPoop <afrifranck2003@gmail.com>
2022-05-18 10:21:59 +0000

    Les pages on été deplacé dans un dossier pages et les vues modifiés

--- src/front/views.py
+++ src/front/views.py
@@ -22,17 +22,17 @@ def fean_index(request):
         "contact_us": ContactUs.objects.first(),
         "link_reseau": LinkReseau.objects.first(),
     }
-    return render(request, "front/index.html", context=data)
+    return render(request, "front/pages/index.html", context=data)
+   
     
 def fean_about(request):
-    return render(request, "front/about.html", context={"about": AboutIndex.objects.first()})
+    return render(request, "front/pages/about.html", context={"about": AboutIndex.objects.first()})
     
     
 def fean_menu(request):
-    menus = AddMenu.objects.all()
-    return render(request, "front/menu.html", context={"menus": menus})
+    return render(request, "front/pages/menu.html", context={"menus": AddMenu.objects.all()})
     
     
 def fean_book(request):
-    return render(request, "front/book.html")
+    return render(request, "front/pages/book.html")
     
